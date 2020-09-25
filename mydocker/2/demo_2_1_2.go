6176ebde0813ee6061c69e7b9915734fc6dea4e3
wangli <wangli@tencent.com>
2020-09-25 22:20:37 +0800

    update;

--- mydocker/2/demo_2_1_2.go
+++ mydocker/2/demo_2_1_2.go
@@ -1,4 +1,20 @@
 package main
 import (
-
+	"os/exec"
+	"syscall"
+	"os"
+	"log"
 )
+
+func main() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscakk.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
