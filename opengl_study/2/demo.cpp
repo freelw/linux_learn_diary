c1565f3e2af1946810305a3e9f65923d9121bddc
freelw <826231693@qq.com>
2022-06-18 20:51:14 +0800

    up

--- opengl_study/2/demo.cpp
+++ opengl_study/2/demo.cpp
@@ -30,16 +30,28 @@ int main(void) {
     fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    float positions[6] = {
+        -0.5f,
+        -0.5f,
+        -0.0f,
+        0.5f,
+        0.5f,
+        -0.5f,
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         // std::cout << "2" << std::endl;
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.51f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
