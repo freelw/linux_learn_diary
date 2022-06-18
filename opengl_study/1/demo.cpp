a59cbcac518691db206e25bf6d0269443789ae18
freelw <826231693@qq.com>
2022-06-18 15:46:23 +0800

    up

--- opengl_study/1/demo.cpp
+++ opengl_study/1/demo.cpp
@@ -27,7 +27,7 @@ int main(void) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
         glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(-0.51f, -0.5f);
         glVertex2f(0.0f, 0.5f);
         glVertex2f(0.5f, -0.5f);
         glEnd();
