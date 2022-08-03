723628919332fe81193d46d947bf62c4c65759fa
freelw <freelw81@qq.com>
2022-08-03 22:05:42 +0800

    up

--- lfs/get_file_name.py
+++ lfs/get_file_name.py
@@ -1,5 +1,6 @@
-input = 'wget-list'
-prefix = 'https://lfs-1252366230.cos.ap-beijing.myqcloud.com/'
+input = 'wget-list-11.1'
+prefix = 'https://lfs-11-1-1259536604.cos.ap-beijing.myqcloud.com/'
+#prefix = 'http://mirrors.ustc.edu.cn/lfs/lfs-packages/11.1/'
 for line in open(input):
     arr = line.strip().split('/')
     print (prefix + arr[-1])
