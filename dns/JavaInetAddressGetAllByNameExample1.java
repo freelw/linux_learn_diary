bfecccfa8fd9b93897e81c67886dea69ce1b1418
freelw <freelw81@qq.com>
2022-09-06 22:16:13 +0800

    up

--- dns/JavaInetAddressGetAllByNameExample1.java
+++ dns/JavaInetAddressGetAllByNameExample1.java
@@ -1,12 +1,14 @@
-import java.net.InetAddress;  
-public class JavaInetAddressGetAllByNameExample1 extends Thread {  
+import java.net.InetAddress;
+import java.util.concurrent.TimeUnit;
+public class JavaInetAddressGetAllByNameExample1 {  
     public static void main (String[] args) {
         try {  
             while (true) {
                 InetAddress[] Addresses =  InetAddress.getAllByName("f11.maxxipoint.com");  
+                System.out.println("123");
                 for (InetAddress address : Addresses)  
                     System.out.println(address.getHostAddress());
-                Thread.sleep(1000);
+                TimeUnit.MILLISECONDS.sleep(1000);
             }
         }  
         catch (Exception e) {  
