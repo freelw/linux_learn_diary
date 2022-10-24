a16bb2203e9cf19fb5a73d24ca50f213ff7a7116
freelw <freelw81@qq.com>
2022-10-24 11:06:14 +0800

    up

--- tcpdump/bond_monitor/log_bond_stat.sh
+++ tcpdump/bond_monitor/log_bond_stat.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+source ./monitorrc
 while /bin/true; do
 date
-cat /proc/net/bonding/bond1 
+echo 'bonding stat:'
+cat /sys/class/net/bond1/bonding/slaves 
+echo "${ETH0NAME} stat:"
+cat /sys/class/net/${ETH0NAME}/bonding_slave/state
+echo "${ETH1NAME} stat:"
+cat /sys/class/net/${ETH1NAME}/bonding_slave/state
 sleep 1
 done
