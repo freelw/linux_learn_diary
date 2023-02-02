35695c6b112ba4a79faf6994009eefbded1c4d6f
freelw <freelw81@qq.com>
2023-02-02 18:55:27 +0800

    up

--- sched_analyze/work.py
+++ sched_analyze/work.py
@@ -1,7 +1,8 @@
+import sys
 from datetime import datetime
 
 def calc_date(t):
-    boot_time = 1666330944.762007849
+    boot_time = 1630049638.372647492
     """
         date | cat > /dev/null # Make sure programs are in cache
         set -- $(date +%s.%N) $(cat /proc/uptime)
@@ -14,29 +15,37 @@ def main():
     index = 0
     s = 0
     interval = 1000 #ms
+    thres = 100
     dic = {}
     diff = 0
-    for line in open(input):
+    details = []
+    #for line in open(input):
+    for line in sys.stdin:
         try:
             arr = line.split(':')
-            #print line.split()
             pid = line.split()[6]
             t = int(float(arr[0].split(' ')[-1])*1000)
             cindex = t / interval
             t1 = cindex*interval/1000.
+            details.append(line)
             if cindex != index:
-                print calc_date(t1), ' ', t1, 'per', interval, 'ms:', s, ' diff cnt: ', diff
+                if diff > thres:
+                    print calc_date(t1), ' ', t1, 'per', interval, 'ms:', s, ' diff cnt: ', diff
+                    print '---------- detail begin ----------------'
+                    for process in details:
+                        print process
+                    print '---------- detail end ------------------'
                 s = 0
                 diff = 0
                 index = cindex
                 dic = {}
+                details = []
             else:
                 if not dic.get(pid, False):
                     dic[pid] = True
                     diff += 1
                 s += 1
         except Exception, e:
-            # print e
             pass
 
 if '__main__' == __name__:
