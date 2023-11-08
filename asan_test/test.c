2ea9ff2e9da0c4ae7f74ae6bb7a421bec26403ea
freelw <freelw81@qq.com>
2023-11-08 17:38:51 +0800

    update

--- asan_test/test.c
+++ asan_test/test.c
@@ -7,9 +7,31 @@ char* getMemory()
     return p;
 }
 
-int main()
-{
+void leak() {
+
     char *p = getMemory();
     p = NULL;
+}
+
+
+void freeagain(char *p) {
+    free(p);
+}
+
+void doublefree() {
+    char *p = malloc(10);
+    free(p);
+    for (int i = 0; i < 10; ++ i) {
+        printf("123\n");
+    }
+    for (int i = 0; i < 10; ++ i) {
+        printf("1235\n");
+    }
+    freeagain(p);
+}
+
+int main()
+{
+    doublefree();
     return 0;
 }
